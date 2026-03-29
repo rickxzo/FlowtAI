b72b60794506e07953ac67cca3a9999ce02901f6
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-29 21:23:20 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 // Lazy-loaded pages (IMPORTANT for performance)
-const Home = () => import(/* webpackPrefetch: true */ '../views/Homme.vue')
-const Login = () => import(/* webpackPrefetch: true */ '../views/Login.vue')
-const Signup = () => import(/* webpackPrefetch: true */ '../views/Signup.vue')
-const Models = () => import (/* webpackPrefetch: true */ '../views/Models.vue')
-const Agents = () => import (/* webpackPrefetch: true */ '../views/Agents.vue')
-const Docs = () => import (/* webpackPrefetch: true */ '../views/Docs.vue')
-const Billing = () => import (/* webpackPrefetch: true */ '../views/Billing.vue')
+import Home from '../views/Homme.vue'
+import Login from '../views/Login.vue'
+import Signup from '../views/Signup.vue'
+import Models from '../views/Models.vue')
+import Agents from '../views/Agents.vue')
+import Docs from '../views/Docs.vue')
+import Billing from '../views/Billing.vue')
 
 const routes = [
   { path: '/', component: Home },
