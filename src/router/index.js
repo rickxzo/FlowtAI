be79bbeadb4e91f3e965b70d691509e8c3a512d5
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-29 21:18:51 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -2,13 +2,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 // Lazy-loaded pages (IMPORTANT for performance)
-const Home = () => import('../views/Homme.vue')
-const Login = () => import('../views/Login.vue')
-const Signup = () => import('../views/Signup.vue')
-const Models = () => import ('../views/Models.vue')
-const Agents = () => import ('../views/Agents.vue')
-const Docs = () => import ('../views/Docs.vue')
-const Billing = () => import ('../views/Billing.vue')
+const Home = () => import(/* webpackPrefetch: true */ '../views/Homme.vue')
+const Login = () => import(/* webpackPrefetch: true */ '../views/Login.vue')
+const Signup = () => import(/* webpackPrefetch: true */ '../views/Signup.vue')
+const Models = () => import (/* webpackPrefetch: true */ '../views/Models.vue')
+const Agents = () => import (/* webpackPrefetch: true */ '../views/Agents.vue')
+const Docs = () => import (/* webpackPrefetch: true */ '../views/Docs.vue')
+const Billing = () => import (/* webpackPrefetch: true */ '../views/Billing.vue')
 
 const routes = [
   { path: '/', component: Home },
