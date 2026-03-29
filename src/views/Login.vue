2acd1d0419cbcdcaf53afb20481954e97be86566
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-29 21:31:09 +0530

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,4 @@
 <script setup>
-import Navbar from '../components/Navbar.vue'
 
 import { ref } from 'vue'
 
@@ -40,7 +39,6 @@ const login = async () => {
 </script>
 
 <template>
-    <Navbar />
     <div class="min-h-[92vh] w-full flex flex-col lg:flex-row text-white mt-12">
 
   <!-- LEFT SECTION -->
