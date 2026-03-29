4e991b059cfaf9deac86545c91651b090a571296
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-29 20:56:39 +0530

    Update Vite server configuration for host and port

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000
+    host: "0.0.0.0",
+    port: process.env.PORT || 5173
   }
 })
