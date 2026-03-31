f8e1eb9416c9b52fcce0a7493543ee600a8afbdf
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-31 20:26:20 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar.vue'
 </script>
 
 <template>
+  <Navbar />
   <Transition name="fade" mode="out-in">
     <router-view :key="$route.fullPath" />
   </Transition>
