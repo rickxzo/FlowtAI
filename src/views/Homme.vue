ac2deba3f3bf5c7266ae3d23f1d9765a9c0b200e
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-03-31 20:27:52 +0530

    Update Homme.vue

--- src/views/Homme.vue
+++ src/views/Homme.vue
@@ -1,9 +1,7 @@
 <script setup>
 import HelloWorld from '../components/HelloWorld.vue'
-  import Navbar from '../components/Navbar.vue'
 </script>
 
 <template>
-  <Navbar />
   <HelloWorld />
 </template>
