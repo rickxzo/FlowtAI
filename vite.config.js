cdba0934d666d0ee1bd8187e4872df06e3f4a497
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-05-19 20:39:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: process.env.PORT || 5173,
-    allowedHosts: ['flowtai.onrender.com']
+    allowedHosts: ['flowtai-slzo.onrender.com']
   }
 })
